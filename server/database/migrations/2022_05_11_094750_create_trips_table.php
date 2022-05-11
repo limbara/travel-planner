65ed3109fe3f51933eecbfe2604b03d3d8bf7bd2
Nico <limbaranico@yahoo.com>
2022-05-11 22:33:15 +0700

    fix migration trip primary key

--- server/database/migrations/2022_05_11_094750_create_trips_table.php
+++ server/database/migrations/2022_05_11_094750_create_trips_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('trips', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('title', 255);
             $table->text('description')->nullable();
             $table->string('origin', 255);
